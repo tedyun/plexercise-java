2b23068b36ea876632896c0eeb0b0b32976246de
Ted Yun <ted.yun@oracle.com>
2018-02-14 18:55:17 -0500

    adding HeapSort class

---
+++ HeapSort.java
@@ -0,0 +1,18 @@
+class Heap {
+    static int MAX_SIZE = 50;
+    int[] heap;
+
+    public Heap () {
+        heap = new int[MAX_SIZE];
+    }
+}
+
+public class HeapSort {
+
+    /**
+     * Main
+     */
+    public static void main (String[] args) {
+        System.out.println("HeapSort class");
+    }
+}
