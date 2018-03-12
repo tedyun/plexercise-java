3296c6a270ac54ea12f6f8edfbecb073a4866d5b
Ted Yun <ted.yun@oracle.com>
2018-03-11 20:25:35 -0400

    fix indentation

--- ThreeWayRadixQuickSort.java
+++ ThreeWayRadixQuickSort.java
@@ -3,23 +3,23 @@ import java.util.Arrays;
 public class ThreeWayRadixQuickSort {
 
     public static void sort(String[] a) {
-      sort(a, 0, a.length - 1, 0);
+        sort(a, 0, a.length - 1, 0);
     }
-  
+
     private static int charAt(String a, int pos) {
-      if (pos < a.length()) {
-        return (int) a.charAt(pos);
-      } else {
-        return -1;
-      }
+        if (pos < a.length()) {
+            return (int) a.charAt(pos);
+        } else {
+            return -1;
+        }
     }
-  
+
     private static void swap(String[] a, int x, int y) {
-      String temp = a[x];
-      a[x] = a[y];
-      a[y] = temp;
+        String temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
     }
-  
+
     private static void sort(String[] a, int low, int high, int d) {
         if (high <= low) return;
         int lp = low, rp = high;
@@ -28,17 +28,17 @@ public class ThreeWayRadixQuickSort {
     
         // partitioning
         while (i <= rp) {
-          int current = charAt(a[i], d);
-          if (current < pivot) {
-            swap(a, i, lp);
-            i++;
-            lp++;
-          } else if (current > pivot) {
-            swap(a, i, rp);
-            rp--;
-          } else {
-            i++;
-          }
+            int current = charAt(a[i], d);
+            if (current < pivot) {
+                swap(a, i, lp);
+                i++;
+                lp++;
+            } else if (current > pivot) {
+                swap(a, i, rp);
+                rp--;
+            } else {
+                i++;
+            }
         }
     
         // sort "less than" part
@@ -47,11 +47,11 @@ public class ThreeWayRadixQuickSort {
         if (pivot >= 0) sort(a, lp, rp, d + 1);
         // sort "greater than" part
         sort(a, rp + 1, high, d);
-      }
-  
-    public static void main(String[] args) {
-      String[] abc = new String[] {"asdlgjdskfa", "gjfklasdr", "bewjrlkj", "asdjk", "alkdsjf"};
-      sort(abc);
-      System.out.println(Arrays.toString(abc));
     }
-  }
+
+    public static void main(String[] args) {
+        String[] abc = new String[] {"asdlgjdskfa", "gjfklasdr", "bewjrlkj", "asdjk", "alkdsjf"};
+        sort(abc);
+        System.out.println(Arrays.toString(abc));
+    }
+}
