c0f17d3729bb71826ace05602ecc093b167d1f8a
Ted Yun <ted.yun@oracle.com>
2018-04-07 20:09:28 -0400

    minor fix

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,7 +6,7 @@ public class InsertionSort {
      * Sort the array using merge sort algorithm.
      */
     public static void sort(int[] input) {
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
             int j = i - 1;
             while (j >= 0 && input[j] > input[j + 1]) {
                 swap(input, j, j + 1);
